5fe0d4f0e782b905d7297f671a8fc1bb3bd4d43e
Fredpwol <cyborg290@gmail.com>
2021-01-10 20:38:31 +0100

    static configs

--- groups/urls.py
+++ groups/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+
 app_name = 'groups'
 urlpatterns = [
     url(r'^$',views.ListGroups.as_view(),name='all'),
@@ -8,4 +10,4 @@ urlpatterns = [
     url(r'^posts/in(?P<slug>[-\w]+)/$',views.SingleGroup.as_view(),name='single'),
     url(r'^join/(?P<slug>[-\w]+)/$',views.JoinGroup.as_view(),name='join'),
     url(r'^leave/(?P<slug>[-\w]+)/$',views.LeaveGroup.as_view(),name='leave')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
